9031052338737d084cf55a285e74adbfa5961201
BasharAAsabeh <bashar13aref@gmail.com>
2024-10-18 21:47:02 +0300

    Update Selection-Test1.cpp

--- Selection-Test1.cpp
+++ Selection-Test1.cpp
@@ -9,20 +9,14 @@ int main() {
 	int x = c + d;
 	int y = a + b;
 
-	if (b > c && d > a) {
-		if (x > y) {
-			if (c >= 0 && d >= 0) {
-				if (a % 2 == 0) {
-					cout << "Valores aceitos" << endl;
-				}
-			}
-		}
+	if (b > c && d > a && (x > y) && (c > 0 && d > 0) && (a % 2 == 0)) {
+		cout << "Valores aceitos" << endl;
+
 	}
 	else {
 		cout << "Valores nao aceitos" << endl;
 	}
 
-	exit(0);
 
 	return 0;
 }
